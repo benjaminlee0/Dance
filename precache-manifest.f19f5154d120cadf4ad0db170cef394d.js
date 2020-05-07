793b5886cbeca84200aba158972d3e7c018575e6
benjaminlee0 <benjamin_lee@ucsb.edu>
2020-05-07 15:56:53 -0700

    Updates

---
+++ precache-manifest.f19f5154d120cadf4ad0db170cef394d.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a21b2d8a395e1d85dd42e1be62f0c1",
+    "url": "/Dance/index.html"
+  },
+  {
+    "revision": "ccf64d9d6dd1fc4730ee",
+    "url": "/Dance/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "46bdde418d645144a6f4",
+    "url": "/Dance/static/js/10.35c0b5cd.chunk.js"
+  },
+  {
+    "revision": "2dfcd25fecbf639250a0",
+    "url": "/Dance/static/js/11.0e511243.chunk.js"
+  },
+  {
+    "revision": "6cdad8508a41d661ace4",
+    "url": "/Dance/static/js/12.b2716613.chunk.js"
+  },
+  {
+    "revision": "559b74bc26b7dfea1a64",
+    "url": "/Dance/static/js/13.1fe30ff8.chunk.js"
+  },
+  {
+    "revision": "f766b5bb6916bd79c179",
+    "url": "/Dance/static/js/14.80b8efbb.chunk.js"
+  },
+  {
+    "revision": "c92995428ff4e1a099c6",
+    "url": "/Dance/static/js/15.90a576e0.chunk.js"
+  },
+  {
+    "revision": "7e89a03bacb897e78c47",
+    "url": "/Dance/static/js/16.1c7d31c1.chunk.js"
+  },
+  {
+    "revision": "7e9cc9eaad60119e5475",
+    "url": "/Dance/static/js/17.0b6c8705.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/Dance/static/js/17.0b6c8705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0024771bb24be6caa6e0",
+    "url": "/Dance/static/js/18.024c581e.chunk.js"
+  },
+  {
+    "revision": "7ee3b4a4bc0ca7bec0c3",
+    "url": "/Dance/static/js/19.1854d637.chunk.js"
+  },
+  {
+    "revision": "2d8b2775dd8905318f8a",
+    "url": "/Dance/static/js/20.65e2e590.chunk.js"
+  },
+  {
+    "revision": "5a55b30b0a0a80580aa3",
+    "url": "/Dance/static/js/21.8d6906f0.chunk.js"
+  },
+  {
+    "revision": "1f1e3b332f938526e9a7",
+    "url": "/Dance/static/js/22.63d28fcb.chunk.js"
+  },
+  {
+    "revision": "45a4d880a1d75873cf5e",
+    "url": "/Dance/static/js/23.3743713c.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/Dance/static/js/23.3743713c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a44f5c031b5a1322751",
+    "url": "/Dance/static/js/24.785d5c84.chunk.js"
+  },
+  {
+    "revision": "71e963f4a57ffe4263e9",
+    "url": "/Dance/static/js/3.876cd0c1.chunk.js"
+  },
+  {
+    "revision": "dd464c2f8129cb40e46dbe56c3c3d98d",
+    "url": "/Dance/static/js/3.876cd0c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1448752a0af5c00f6873",
+    "url": "/Dance/static/js/4.2d4f8031.chunk.js"
+  },
+  {
+    "revision": "17c488f283d878296a5f",
+    "url": "/Dance/static/js/5.b6066b35.chunk.js"
+  },
+  {
+    "revision": "ae503e229f72dd0ab65f",
+    "url": "/Dance/static/js/6.bb3f0724.chunk.js"
+  },
+  {
+    "revision": "cddc28501b26d8015874",
+    "url": "/Dance/static/js/7.71270477.chunk.js"
+  },
+  {
+    "revision": "96c1b5575c82f91ee9a1",
+    "url": "/Dance/static/js/8.7c334d46.chunk.js"
+  },
+  {
+    "revision": "a2eb9188bf3e2d360e3e",
+    "url": "/Dance/static/js/9.47f83d94.chunk.js"
+  },
+  {
+    "revision": "ccf64d9d6dd1fc4730ee",
+    "url": "/Dance/static/js/main.f64fb16b.chunk.js"
+  },
+  {
+    "revision": "f9a8d3f18f90b3ded431",
+    "url": "/Dance/static/js/polyfills-css-shim.ee1abc1f.chunk.js"
+  },
+  {
+    "revision": "a54b3d4c9f5ed350bcac",
+    "url": "/Dance/static/js/runtime-main.be6c5104.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Dance/static/media/logo.5d5d9eef.svg"
+  }
+]);
